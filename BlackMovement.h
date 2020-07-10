11633f44dfa894664f35afd4bd26dc5fa3431561
Erkhembaatar Dashdorj <erhembaatardashdorj@gmail.com>
2020-07-10 15:41:34 +0800

    Changed header files

--- BlackMovement.h
+++ BlackMovement.h
@@ -4,3 +4,5 @@ int moveBlackBishop(int board[8][8], int x, int y);
 int moveBlackHorse(int board[8][8], int x, int y);
 int moveBlackQueen(int board[8][8], int x, int y);
 int moveBlackKing(int board[8][8], int x, int y);
+int moveBlackQueenBishop(int board[8][8], int x, int y, int newX, int newY);
+int moveBlackQueenRook(int board[8][8], int x, int y, int newX, int newY);
--- WhiteMovement.h
+++ WhiteMovement.h
@@ -4,3 +4,5 @@ int moveWhiteBishop(int board[8][8], int x, int y);
 int moveWhiteHorse(int board[8][8], int x, int y);
 int moveWhiteQueen(int board[8][8], int x, int y);
 int moveWhiteKing(int board[8][8], int x, int y);
+int moveWhiteQueenRook(int board[8][8], int x, int y, int newX, int newY);
+int moveWhiteQueenBishop(int board[8][8], int x, int y, int newX, int newY);
